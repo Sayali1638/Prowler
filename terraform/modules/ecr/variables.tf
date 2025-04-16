fd88c92438b0f9f2ea80282fd63304038212eb57
Sayali1638 <66510021+Sayali1638@users.noreply.github.com>
2025-04-16 21:26:28 +0530

    Add files via upload

---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,21 @@
+variable "environment" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+}
+
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "scan_on_push" {
+  description = "Indicates whether images are scanned after being pushed to the repository"
+  type        = bool
+  default     = true
+} 
